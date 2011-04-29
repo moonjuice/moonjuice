2fb685d2db564a3f49b7a11db7d5690bba58e74a
moon80362@gmail.com <moon80362@gmail.com@8026b7c3-d9ae-87c5-0fde-12bd377d1155>
2011-04-29 09:58:08 +0000

    完成POST動作來登入，但是需要開啟COOKIE!!

---
+++ AutoLike/AutoLike/AutoLike/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AutoLike
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultText = new System.Windows.Forms.RichTextBox();
+            this.enter = new System.Windows.Forms.Button();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // resultText
+            // 
+            this.resultText.Location = new System.Drawing.Point(13, 41);
+            this.resultText.Name = "resultText";
+            this.resultText.Size = new System.Drawing.Size(342, 270);
+            this.resultText.TabIndex = 0;
+            this.resultText.Text = "";
+            // 
+            // enter
+            // 
+            this.enter.Location = new System.Drawing.Point(326, 11);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(29, 23);
+            this.enter.TabIndex = 1;
+            this.enter.Text = "Go";
+            this.enter.UseVisualStyleBackColor = true;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(13, 13);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(307, 22);
+            this.urlText.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 323);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.resultText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox resultText;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.TextBox urlText;
+    }
+}
+
---
+++ AutoLike/AutoLike/AutoLike/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Collections.Specialized;
+
+namespace AutoLike
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void enter_Click(object sender, EventArgs e)
+        {
+            String url = urlText.Text;
+            WebClient client = new WebClient();
+            NameValueCollection myNameValueCollection = new NameValueCollection();
+            myNameValueCollection.Add("charset_test", "&euro;,&acute;,€,’,水,?,?");
+            myNameValueCollection.Add("lsd", "wVKoH");
+            myNameValueCollection.Add("locale", "zh_TW");
+            myNameValueCollection.Add("email", "fishbowformoon@gmail.com");
+            myNameValueCollection.Add("pass", "761280362");
+            url = "https://www.facebook.com/login.php?login_attempt=1";
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            //Stream data = client.OpenRead(url);
+            //StreamReader reader = new StreamReader(data);
+            //string s = reader.ReadToEnd();
+            //resultText.Text = s;
+            //data.Close();
+            //reader.Close();
+            byte[] responseArray = client.UploadValues(url, myNameValueCollection);
+            resultText.Text = Encoding.UTF8.GetString(responseArray);
+        }
+    }
+}
