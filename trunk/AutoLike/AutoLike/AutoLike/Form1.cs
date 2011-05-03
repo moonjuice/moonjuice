4ad6dc48d24a00ff7f1a7ca7e01ea263583518cd
moon80362@gmail.com <moon80362@gmail.com@8026b7c3-d9ae-87c5-0fde-12bd377d1155>
2011-05-03 08:18:54 +0000

    可以登入

--- trunk/AutoLike/AutoLike/AutoLike/Form1.cs
+++ trunk/AutoLike/AutoLike/AutoLike/Form1.cs
@@ -21,6 +21,7 @@ namespace AutoLike
 
         private void enter_Click(object sender, EventArgs e)
         {
+            /*
             String url = urlText.Text;
             WebClient client = new WebClient();
             NameValueCollection myNameValueCollection = new NameValueCollection();
@@ -30,6 +31,7 @@ namespace AutoLike
             myNameValueCollection.Add("email", "fishbowformoon@gmail.com");
             myNameValueCollection.Add("pass", "761280362");
             url = "https://www.facebook.com/login.php?login_attempt=1";
+            //url = "https://www.facebook.com/";
             client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
             //Stream data = client.OpenRead(url);
             //StreamReader reader = new StreamReader(data);
@@ -39,6 +41,73 @@ namespace AutoLike
             //reader.Close();
             byte[] responseArray = client.UploadValues(url, myNameValueCollection);
             resultText.Text = Encoding.UTF8.GetString(responseArray);
+            */
+
+            var cookies = new CookieContainer();
+            Cookie c = new Cookie("locale", "zh_TW");
+            cookies.Add(new Uri("https://www.facebook.com/"), c);
+            ServicePointManager.Expect100Continue = false;
+
+            //var request = (HttpWebRequest)WebRequest.Create("https://www.facebook.com/");
+            var request = (HttpWebRequest)WebRequest.Create("https://www.facebook.com/login.php?login_attempt=1");
+            request.CookieContainer = cookies;
+            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)";
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            using (var requestStream = request.GetRequestStream())
+            using (var writer = new StreamWriter(requestStream))
+            {
+                writer.Write("charset_test=&euro;,&acute;,€,’,水,?,?&lsd=wVKoH&locale=zh_TW&email=fishbowformoon@gmail.com&pass=761280362");
+            }
+
+            using (var responseStream = request.GetResponse().GetResponseStream())
+            using (var reader = new StreamReader(responseStream, Encoding.UTF8))
+            {
+                var result = reader.ReadToEnd();
+                //resultText.Text = (result);
+            }
+            resultText.Text = cookies.GetCookies(new Uri("http://www.facebook.com")).Count + "";
+            foreach (Cookie cookie in cookies.GetCookies(new Uri("http://www.facebook.com")))
+            {
+                resultText.Text += cookie.Name + ":" + cookie.Value;
+            }
+
+//             CookieContainer cookieContainer = new CookieContainer();
+//             NameValueCollection myNameValueCollection = new NameValueCollection();
+//             myNameValueCollection.Add("charset_test", "&euro;,&acute;,€,’,水,?,?");
+//             myNameValueCollection.Add("lsd", "wVKoH");
+//             myNameValueCollection.Add("locale", "zh_TW");
+//             myNameValueCollection.Add("email", "fishbowformoon@gmail.com");
+//             myNameValueCollection.Add("pass", "761280362");
+// 
+// 
+//             UTF8Encoding encoding = new UTF8Encoding();
+//             string postData = "";
+//             for (int i = 0; i < myNameValueCollection.Count; i++)
+//                 postData += myNameValueCollection.GetKey(i) + "=" + myNameValueCollection.Get(i) + "&";
+// 
+//             byte[] data = encoding.GetBytes(postData);
+// 
+//             // Prepare web request...
+//             HttpWebRequest myRequest =
+//             (HttpWebRequest)WebRequest.Create("https://www.facebook.com/login.php?login_attempt=1");
+// 
+//             myRequest.CookieContainer = cookieContainer;
+//             myRequest.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)";
+//             myRequest.Method = "POST";
+//             myRequest.ContentType = "application/x-www-form-urlencoded";
+//             myRequest.ContentLength = data.Length;
+//             Stream newStream = myRequest.GetRequestStream();
+// 
+//             // Send the data.
+//             newStream.Write(data, 0, data.Length);
+//             newStream.Close();
+// 
+//             // Get response
+//             HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+//             StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.UTF8);
+//             string content = reader.ReadToEnd();
+//             resultText.Text = (content);
         }
     }
 }
---
+++ trunk/AutoLike/AutoLike/AutoLike/HttpClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Net;
+
+
+namespace AutoLike
+{
+    class HttpClient : WebClient
+    {
+
+        private CookieContainer cookieContainer;
+
+
+        public HttpClient()
+        {
+            this.cookieContainer = new CookieContainer();
+        }
+
+
+        public HttpClient(CookieContainer cookies)
+        {
+            this.cookieContainer = cookies;
+        }
+
+
+        public CookieContainer Cookies
+        {
+            get { return this.cookieContainer; }
+            set { this.cookieContainer = value; }
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                HttpWebRequest httpRequest = request as HttpWebRequest;
+                httpRequest.CookieContainer = cookieContainer;
+            }
+            return request;
+        }
+    }
+}
