4ebde69b039006994ac2105018d5a92957247b51
moon80362@gmail.com <moon80362@gmail.com@8026b7c3-d9ae-87c5-0fde-12bd377d1155>
2011-05-12 09:16:23 +0000

    submit

---
+++ trunk/AutoVote/AutoVote/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AutoVote
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.phoneText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.mailText = new System.Windows.Forms.TextBox();
+            this.vote = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.RichTextBox();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.ColumnCount = 3;
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.30986F));
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.85915F));
+            this.mainPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.1831F));
+            this.mainPanel.Controls.Add(this.label1, 0, 0);
+            this.mainPanel.Controls.Add(this.nameText, 1, 0);
+            this.mainPanel.Controls.Add(this.label2, 0, 1);
+            this.mainPanel.Controls.Add(this.phoneText, 1, 1);
+            this.mainPanel.Controls.Add(this.label3, 0, 2);
+            this.mainPanel.Controls.Add(this.mailText, 1, 2);
+            this.mainPanel.Controls.Add(this.vote, 2, 0);
+            this.mainPanel.Controls.Add(this.result, 0, 3);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.RowCount = 4;
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.45038F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.68702F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.45038F));
+            this.mainPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 67.17558F));
+            this.mainPanel.Size = new System.Drawing.Size(284, 262);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nameText
+            // 
+            this.nameText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nameText.Location = new System.Drawing.Point(54, 3);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(163, 22);
+            this.nameText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "手機：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // phoneText
+            // 
+            this.phoneText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.phoneText.Location = new System.Drawing.Point(54, 32);
+            this.phoneText.Name = "phoneText";
+            this.phoneText.Size = new System.Drawing.Size(163, 22);
+            this.phoneText.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(3, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "mail：";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mailText
+            // 
+            this.mailText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mailText.Location = new System.Drawing.Point(54, 59);
+            this.mailText.Name = "mailText";
+            this.mailText.Size = new System.Drawing.Size(163, 22);
+            this.mailText.TabIndex = 5;
+            // 
+            // vote
+            // 
+            this.vote.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.vote.Location = new System.Drawing.Point(223, 3);
+            this.vote.Name = "vote";
+            this.mainPanel.SetRowSpan(this.vote, 3);
+            this.vote.Size = new System.Drawing.Size(58, 79);
+            this.vote.TabIndex = 6;
+            this.vote.Text = "投票";
+            this.vote.UseVisualStyleBackColor = true;
+            this.vote.Click += new System.EventHandler(this.vote_Click);
+            // 
+            // result
+            // 
+            this.mainPanel.SetColumnSpan(this.result, 3);
+            this.result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.result.Location = new System.Drawing.Point(3, 88);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(278, 171);
+            this.result.TabIndex = 7;
+            this.result.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
+            this.Text = "自動投票系統";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel mainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox phoneText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox mailText;
+        private System.Windows.Forms.Button vote;
+        private System.Windows.Forms.RichTextBox result;
+    }
+}
+
---
+++ trunk/AutoVote/AutoVote/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using System.Threading;
+
+namespace AutoVote
+{
+    public partial class Form1 : Form
+    {
+        private voteWork workerObject;
+        private Thread workerThread;
+        public Form1()
+        {
+            InitializeComponent();
+            workerObject = new voteWork(result, nameText.Text, phoneText.Text, mailText.Text);
+            workerThread = new Thread(workerObject.DoWork);
+        }
+        class voteWork
+        {
+            private RichTextBox result;
+            private String name, phone, mail;
+            private volatile bool _shouldStop = false;
+            public voteWork(RichTextBox r,String n,String p,String m)
+            {
+                this.result = r;
+                this.name = n;
+                this.mail = m;
+                this.phone = p;
+            }
+            public void DoWork()
+            {
+                 while (!_shouldStop)
+                 {
+                    if (voteFunction())
+                        waitMinute(60);
+                    else
+                        waitMinute(15);
+                 }
+            }
+            public void RequestStop()
+            {
+                _shouldStop = true;
+#if DEBUG
+                result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("_shouldStop : " + _shouldStop.ToString() + "\n"); }));
+#endif
+            }
+            private bool voteFunction()
+            {
+                System.Net.ServicePointManager.Expect100Continue = false;//解決417錯誤
+                DateTime date = DateTime.Now;
+                String dateString;
+                String html, verifyCode;
+                //取得時間
+                dateString = date.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo);
+                result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("取得時間 : " + dateString + "\n"); }));
+                //取得本機IP，有可能為虛擬IP
+                IPAddress myIP = new IPAddress(Dns.GetHostByName(Dns.GetHostName()).AddressList[0].Address);
+                result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("取得IP : " + myIP.ToString() + "\n"); }));
+                //取得驗證碼            
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://events.fashionguide.com.tw/2011mucha/vote_process.php?seqno=28&photo_half=f63288d6c6f8d60696e3ca0203bbc11d.jpg&counter=87");
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24";
+                request.Method = "GET";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.KeepAlive = true;
+                using (var responseStream = request.GetResponse().GetResponseStream())
+                using (var reader = new StreamReader(responseStream, Encoding.UTF8))
+                {
+                    html = reader.ReadToEnd();
+                }
+                verifyCode = html.Substring(html.IndexOf("==") + 3, 5);
+                result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("取得驗證碼 : " + verifyCode + "\n"); }));
+                //投票
+                request.AllowAutoRedirect = false;
+                request = (HttpWebRequest)WebRequest.Create("http://events.fashionguide.com.tw/2011mucha/vote_process.php?seqno=28&photo_half=f63288d6c6f8d60696e3ca0203bbc11d.jpg&counter=93");
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.65 Safari/534.24";
+                request.Method = "POST";
+                request.KeepAlive = true;
+                request.ContentType = "application/x-www-form-urlencoded";
+                using (var requestStream = request.GetRequestStream())
+                using (var writer = new StreamWriter(requestStream))
+                {
+                    writer.Write("voterName=" + name + "&candidate=28&IP=" + myIP.ToString() + "&voteTime=" + dateString + "&createDate=" + dateString + "&cellphone=" + phone + "&email=" + mail + "&newsCode=on&verifyCode=" + verifyCode + "&button=送出&MM_insert=formVote");
+                }
+                using (var responseStream = request.GetResponse().GetResponseStream())
+                using (var reader = new StreamReader(responseStream, Encoding.UTF8))
+                {
+                    html = reader.ReadToEnd();
+                }
+                if (html.Contains("vote_fail"))
+                {
+                    result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("投票失敗!!\n"); }));
+                    return false;
+                }
+                else
+                {
+                    result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("投票成功!!\n"); }));
+                    return true;
+                }
+            }
+            private void waitMinute(int m)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    if (this._shouldStop)
+                        break;
+                    else
+                    {
+                        result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("倒數" + (m - i) + "分鐘再投票一次!!\n"); }));
+                        for (int j = 0; j < 60000; j++)
+                        {
+                            if (this._shouldStop)
+                            {
+                                result.BeginInvoke(new MethodInvoker(delegate { result.AppendText("已停止!!\n"); }));
+                                break;
+                            }
+                            else
+                                Thread.Sleep(1);
+                        }
+                    }
+                }
+            }
+        }
+        private void vote_Click(object sender, EventArgs e)
+        {
+#if DEBUG
+            nameText.Text = "李友謙";
+            mailText.Text = "jerry751122@gmail.com";
+            phoneText.Text = "0930022177";
+#endif
+            // Create the thread object. This does not start the thread.
+            if (vote.Text.Equals("投票"))
+            {
+                vote.Text = "停止";                
+                // Start the worker thread.
+                workerThread.IsBackground = true;
+                workerThread.Start();
+                while (!workerThread.IsAlive) ;
+            } 
+            else
+            {
+                vote.Text = "投票";
+                if (workerThread.IsAlive)
+                {
+                    workerObject.RequestStop();
+                    workerThread.Join();
+                }
+            }
+        }
+    }
+}
