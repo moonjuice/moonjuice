79c1fdfc695f55c80cada2deb7a97f3bfe9b8cb7
moon80362@gmail.com <moon80362@gmail.com@8026b7c3-d9ae-87c5-0fde-12bd377d1155>
2011-05-19 06:44:47 +0000

    UI completed

---
+++ trunk/EzQuickReply/EzQuickReply/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace EzQuickReply
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.passwordText = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.postList = new System.Windows.Forms.ListBox();
+            this.replyList = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.add = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.del = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.statusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.status.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.IDText, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.passwordText, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.login, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.postList, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.replyList, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(484, 460);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "帳號：";
+            // 
+            // IDText
+            // 
+            this.IDText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.IDText.Location = new System.Drawing.Point(61, 12);
+            this.IDText.Name = "IDText";
+            this.IDText.Size = new System.Drawing.Size(148, 22);
+            this.IDText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密碼：";
+            // 
+            // passwordText
+            // 
+            this.passwordText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordText.Location = new System.Drawing.Point(273, 12);
+            this.passwordText.Name = "passwordText";
+            this.passwordText.Size = new System.Drawing.Size(148, 22);
+            this.passwordText.TabIndex = 3;
+            // 
+            // login
+            // 
+            this.login.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.login.Location = new System.Drawing.Point(427, 11);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(54, 23);
+            this.login.TabIndex = 4;
+            this.login.Text = "登入";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // postList
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.postList, 5);
+            this.postList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.postList.FormattingEnabled = true;
+            this.postList.ItemHeight = 12;
+            this.postList.Location = new System.Drawing.Point(3, 49);
+            this.postList.Name = "postList";
+            this.postList.Size = new System.Drawing.Size(478, 172);
+            this.postList.TabIndex = 5;
+            // 
+            // replyList
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.replyList, 5);
+            this.replyList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.replyList.FormattingEnabled = true;
+            this.replyList.ItemHeight = 12;
+            this.replyList.Location = new System.Drawing.Point(3, 233);
+            this.replyList.Name = "replyList";
+            this.replyList.Size = new System.Drawing.Size(478, 172);
+            this.replyList.TabIndex = 6;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 5);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.Controls.Add(this.add, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.edit, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.del, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 417);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(478, 40);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // add
+            // 
+            this.add.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.add.Location = new System.Drawing.Point(3, 3);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(153, 34);
+            this.add.TabIndex = 0;
+            this.add.Text = "新增回復";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // edit
+            // 
+            this.edit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.edit.Location = new System.Drawing.Point(162, 3);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(153, 34);
+            this.edit.TabIndex = 1;
+            this.edit.Text = "修改回復";
+            this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // del
+            // 
+            this.del.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.del.Location = new System.Drawing.Point(321, 3);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(154, 34);
+            this.del.TabIndex = 2;
+            this.del.Text = "刪除回復";
+            this.del.UseVisualStyleBackColor = true;
+            this.del.Click += new System.EventHandler(this.del_Click);
+            // 
+            // status
+            // 
+            this.status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusText});
+            this.status.Location = new System.Drawing.Point(0, 460);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(484, 22);
+            this.status.TabIndex = 1;
+            this.status.Text = "目前狀態";
+            // 
+            // statusText
+            // 
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(92, 17);
+            this.statusText.Text = "目前狀態：閒置";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 482);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.status);
+            this.Name = "Form1";
+            this.Text = "依莉論壇快速回復系統";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.status.ResumeLayout(false);
+            this.status.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IDText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox passwordText;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.ListBox postList;
+        private System.Windows.Forms.ListBox replyList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.ToolStripStatusLabel statusText;
+    }
+}
+
