32e6c533e7ac22f6f6de9f637b1d6f80f06099d9
moon80362 <moon80362@8026b7c3-d9ae-87c5-0fde-12bd377d1155>
2011-10-07 08:20:29 +0000

    完成登入功能

--- trunk/EzQuickReply/EzQuickReply/Program.cs
+++ trunk/EzQuickReply/EzQuickReply/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.IO;
 
 namespace EzQuickReply
 {
@@ -13,9 +16,43 @@ namespace EzQuickReply
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            ServicePointManager.Expect100Continue = false;
+            //string strId = "moonjuice";
+            //string strPassword = "teeth7612";
+
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            //string postData = "userid=" + strId;
+            //postData += ("&password=" + strPassword);
+            string postData;
+            postData =("formhash=5027383b&referer=index.php&loginfield=username&username=moonjuice&password=teeth7612&loginsubmit=true&cookietime=2592000");
+
+            byte[] data = encoding.GetBytes(postData);
+
+            CookieContainer cookieContainer = new CookieContainer();
+
+            // Prepare web request...  
+            HttpWebRequest myRequest =
+            (HttpWebRequest)WebRequest.Create("http://www.eyny.com/logging.php?action=login&amp;loginsubmit=yes");
+
+            myRequest.CookieContainer = cookieContainer;
+
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/x-www-form-urlencoded";
+            myRequest.ContentLength = data.Length;
+            Stream newStream = myRequest.GetRequestStream();
+
+            // Send the data.  
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+
+            // Get response  
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.Default);
+            string content = reader.ReadToEnd();
+            Console.WriteLine(content);
         }
     }
 }
