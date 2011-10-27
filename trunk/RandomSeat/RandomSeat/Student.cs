744c9b89742fb88d7d17df5e0f53e13ae19b705d
moon80362 <moon80362@8026b7c3-d9ae-87c5-0fde-12bd377d1155>
2011-10-27 10:09:08 +0000

    add RandomSeat

---
+++ trunk/RandomSeat/RandomSeat/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RandomSeat
+{
+    class Student
+    {
+        private string m_Name;
+        private string m_ID;
+
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public string ID
+        {
+            get { return m_ID; }
+            set { m_ID = value; }
+        }
+    }
+}
